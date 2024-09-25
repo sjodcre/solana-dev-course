3cbacf89e1470e647beaa854f8947a0613d5b35d
Your Name <you@example.com>
2024-09-25 12:31:38 +0800

    initial commit and lab 1

---
+++ generate-keypair.ts
@@ -0,0 +1,23 @@
+// import { addKeypairToEnvFile } from "@solana-developers/helpers";
+// import { Keypair } from "@solana/web3.js";
+// import dotenv from 'dotenv';
+// dotenv.config();
+ 
+// const keypair = Keypair.generate();
+ 
+// console.log(`The public key is: `, keypair.publicKey.toBase58());
+// console.log(`The secret key is: `, keypair.secretKey);
+// console.log(`✅ Finished!`);
+
+// await addKeypairToEnvFile(keypair, "SECRET_KEY_DEMO", ".env");
+
+
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+ 
+// const keypair = getKeypairFromEnvironment("SECRET_KEY");
+const keypair = await getKeypairFromEnvironment("SECRET_KEY");
+ 
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`,
+);
